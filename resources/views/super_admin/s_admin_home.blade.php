7896cbb077cfb9d0dcc2c1dff117e3fb9d20f377
rayxCode <yukixray124@gmail.com>
2024-02-26 14:00:33 +0800

    2nd Update
    dataTables

---
+++ resources/views/super_admin/s_admin_home.blade.php
@@ -0,0 +1,83 @@
+@extends('layout.app')
+
+
+@section('styles')
+<link rel="stylesheet" href="{{asset('css/datatables/datatable_v2.css')}}">
+@endsection
+
+@section('body-content')
+    <div class="contentcon">
+        <div class="box-con">
+            <div class="table-box">
+                <div class="table-wrap">
+                    <table id="dataTable" class="table-content display">
+                        <thead>
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                                <th>Column 4</th>
+                                <th>Column 5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Data 1</td>
+                                <td>Data 2</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                            </tr>
+                            <tr>
+                                <td>Data 1</td>
+                                <td>Data 2</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                            </tr>
+                            <tr>
+                                <td>Data 1</td>
+                                <td>Data 2</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                            </tr>
+                            <tr>
+                                <td>Data 1</td>
+                                <td>Data 2</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                            </tr>
+                            <tr>
+                                <td>Data 1</td>
+                                <td>Data 2</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('scripts')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        // Your code to be executed when the DOM is ready
+        $('#dataTable').DataTable({
+            "lengthChange": false,
+            "pageLength": 10,
+            "searching": true
+        });
+
+    });
+</script>
+@endsection
