8382554ed3b0491c1b8656d1a3e48a1741a3f0b6
maxis21 <max.esconde@gmail.com>
2024-02-27 13:50:32 +0800

    Dashboard Update

--- resources/views/all/dashboard.blade.php
+++ resources/views/all/dashboard.blade.php
@@ -2,33 +2,52 @@
 @section('title', 'Home')
 
 @section('styles')
-    <!-- <link rel="stylesheet" href="{{ asset('css/datatables/jquery.dataTables.css') }}">
+<!-- <link rel="stylesheet" href="{{ asset('css/datatables/jquery.dataTables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/datatables/datatable_v2.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 @endsection
 
 @section('body-content')
-    <!-- -->
-    <div class="contentcon">
-        <div class="container-fluid d-flex" style="justify-content: space-between;">
-            <div style="display: flex; justify-content: space-between; align-items: center; width: 100%">
-                <p style="color: grey"><b>Dashboard</b></p>
-                <ul class="breadcrumbs">
-                    <li class="text-white"><a href="#">Dashboard</a></li>
-                    <!-- <li class="text-white active"> Deparments </li> -->
-                </ul>
-            </div>
-        </div>          
-        <!-- -->
-
-        <!-- -->
+<!-- -->
+<div class="contentcon">
+    <div class="container-fluid d-flex" style="justify-content: space-between;">
+        <div style="display: flex; justify-content: space-between; align-items: center; width: 100%">
+            <p style="color: grey"><b>Dashboard</b></p>
+            <ul class="breadcrumbs">
+                <li class="text-white"><a href="#">Dashboard</a></li>
+                <!-- <li class="text-white active"> Deparments </li> -->
+            </ul>
+        </div>
     </div>
     <!-- -->
+    <div class="body-container container-fluid d-flex">
+        <div class="card-group d-flex">
+            <div class="dash-card">
+                1
+            </div>
+            <div class="dash-card">
+                2
+            </div>
+            <div class="dash-card">
+                3
+            </div>
+            <div class="dash-card">
+                4
+            </div>
+        </div>
+        <div class="body-content">
+
+        </div>
+    </div>
+    <!-- -->
+</div>
+<!-- -->
 @endsection
 
 
 @section('scripts')
-    <!-- <script src="{{ asset('js/datatables/jquery.dataTables.js') }}"></script>
+<!-- <script src="{{ asset('js/datatables/jquery.dataTables.js') }}"></script>
 
     <script>
         $(document).ready(function() {
