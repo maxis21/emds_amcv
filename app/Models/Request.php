e87d69b9a96a4ac97eea948827eeb4ccd0bffac6
maxis21 <max.esconde@gmail.com>
2024-02-28 11:20:34 +0800

    Models and Relationships
    Added models and relationships to tables.

---
+++ app/Models/Request.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_requests';
+
+    protected $fillable = [
+        'user_id',
+        'document_id',
+        'request_status',
+        'file_url',
+    ];
+
+    public function document(){
+        return $this->hasOne(Document::class);
+    }
+
+    public function user(){
+        return $this->hasOne(User::class);
+    }
+
+    public function transaction(){
+        return $this->belongsTo(transaction::class);
+    }
+}
