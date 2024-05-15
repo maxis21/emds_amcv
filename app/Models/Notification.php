2bc72980e7a06aca662a4899542f6fd55c3a595c
maxis21 <max.esconde@gmail.com>
2024-05-15 11:33:50 +0800

    Added Notification Table

---
+++ app/Models/Notification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notifications';
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'status',
+        'document_id',
+        'message',
+        'is_read'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function documents()
+    {
+        return $this->belongsTo(DocumentVersion::class);
+    }
+}
