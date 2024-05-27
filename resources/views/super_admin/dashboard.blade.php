806e46aa36a11b2c937025d1cbedb0a0142ba7af
maxis21 <max.esconde@gmail.com>
2024-05-27 11:44:06 +0800

    PUSGUIASD

--- resources/views/super_admin/dashboard.blade.php
+++ resources/views/super_admin/dashboard.blade.php
@@ -28,63 +28,14 @@
             </ul>
         </div>
         <!-- -->
-        <div class="body-container container-fluid d-flex">
-            <div class="card-group d-flex">
-                <div class="dash-card dash-1">
-                    <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DEPARTMENTS<br><span
-                            style="font-size: 25px;">{{ $totalDpt->count() }}</span> </h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor"
-                        class="bi bi-building-fill card-icon" viewBox="0 0 16 16">
-                        <path
-                            d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5" />
-                    </svg>
-                </div>
-                <div class="dash-card dash-2">
-                    <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">REQUESTS<br><span
-                            style="font-size: 25px;">{{ $totalreq }}</span> </h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor"
-                        class="bi bi-chat-fill card-icon" viewBox="0 0 16 16">
-                        <path
-                            d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
-                    </svg>
-                </div>
-                <div class="dash-card dash-3">
-                    <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DOCUMENTS<br><span
-                            style="font-size: 25px;">{{ $totalDocs->count() }}</span> </h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor"
-                        class="bi bi-folder-fill card-icon" viewBox="0 0 16 16">
-                        <path
-                            d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
-                    </svg>
-                </div>
-                <div class="dash-card dash-4">
-                    <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">TOTAL UPLOADS:<br><span
-                            style="font-size: 25px;">{{ $totalUploads->count() }}</span> </h5>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor"
-                        class="bi bi-building-fill card-icon" viewBox="0 0 16 16">
-                        <path
-                            d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5" />
-                    </svg>
-                </div>
-
-            </div>
-            <div class="body-content d-flex row">
-                <div class="chart1">
-                    <canvas id="myChart" width="1000" height="525"></canvas>
-                </div>
-                &nbsp;
-                <div >
-                    <canvas id="myChart2" width="1000" height="525"></canvas>
-                </div>
-            </div>
-        </div>
+        
         <!-- -->
     </div>
 
     <div class="body-container container-fluid d-flex">
         <div class="card-group d-flex">
             <div class="dash-card dash-1">
-                <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DEPARTMENTS<br><span style="font-size: 25px;">{{ $totalDpt }}</span> </h5>
+                <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DEPARTMENTS<br><span style="font-size: 25px;">{{ $totalDpt->count() }}</span> </h5>
                 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-building-fill card-icon" viewBox="0 0 16 16">
                     <path d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5" />
                 </svg>
@@ -96,7 +47,7 @@
                 </svg>
             </div>
             <div class="dash-card dash-3">
-                <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DOCUMENTS<br><span style="font-size: 25px;">{{ $totalDocs }}</span> </h5>
+                <h5 style="display: flex; flex-direction: column; gap: 0.4rem;">DOCUMENTS<br><span style="font-size: 25px;">{{ $totalDocs->count() }}</span> </h5>
                 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-folder-fill card-icon" viewBox="0 0 16 16">
                     <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
                 </svg>
@@ -109,7 +60,7 @@
             </div>
 
         </div>
-        <!-- <div class="body-content d-flex row">
+        <div class="body-content d-flex row">
             <div class="chart1">
                 <canvas id="myChart" width="1000" height="525"></canvas>
             </div>
@@ -128,7 +79,7 @@
                     <span> number something something </span>
                 </div>
             </div>
-        </div> -->
+        </div>
     </div>
 
     <div class="notif-box d-flex">
@@ -190,7 +141,6 @@
 
 
 @section('scripts')
-<script src="{{ asset('js/chart.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 
